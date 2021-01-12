5cefadd3ba4293a92c6c3acd1474df75de9b2598
peterdee <p.dyumin@dunice.net>
2021-01-12 15:48:22 +0300

    Update the Not Found screen

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -1,26 +1,19 @@
-import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 
+import colors from '../constants/Colors';
 import { RootStackParamList } from '../types';
-
-export default function NotFoundScreen({
-  navigation,
-}: StackScreenProps<RootStackParamList, 'NotFound'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.appBackground,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
@@ -30,11 +23,31 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   link: {
-    marginTop: 15,
-    paddingVertical: 15,
+    marginTop: 16,
+    paddingVertical: 16,
   },
   linkText: {
     fontSize: 14,
-    color: '#2e78b7',
+    color: colors.accent,
   },
 });
+
+export default function NotFoundScreen({
+  navigation,
+}: StackScreenProps<RootStackParamList, 'NotFound'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        This screen doesn't exist.
+      </Text>
+      <Pressable
+        onPress={() => navigation.replace('Root')}
+        style={styles.link}
+      >
+        <Text style={styles.linkText}>
+          Go to home screen!
+        </Text>
+      </Pressable>
+    </View>
+  );
+}
